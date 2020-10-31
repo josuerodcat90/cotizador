fa60fcb49764e2278980dd835ee52da33a61fddc
55036662 <zhippuden@gmail.com>
2020-10-31 17:16:01 +0100

    Changed names to english to prevent language mixes.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import Header from './components/Header';
 
 import styled from '@emotion/styled';
 
-const Contenedor = styled.div`
+const Container = styled.div`
 	max-width: 600px;
 	margin: 0 auto;
 `;
 
-const ContenedorFormulario = styled.div`
+const FormContainer = styled.div`
 	background-color: #ffffff;
 	padding: 3rem;
 `;
 
 function App() {
 	return (
-		<Contenedor>
-			<Header titulo='Cotizador de Seguros' />
+		<Container>
+			<Header title='Cotizador de Seguros' />
 
-			<ContenedorFormulario>
+			<FormContainer>
 				<h1>formulario</h1>
-			</ContenedorFormulario>
-		</Contenedor>
+			</FormContainer>
+		</Container>
 	);
 }
 
